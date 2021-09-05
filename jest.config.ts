b536ba15a8807f1827d1ff711d906b406ee1d76d
Rubens Batista <batistarubinho@gmail.com>
2021-09-04 23:56:57 -0300

    chore: add @testing-library and @testing-library/dom

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,6 @@ export default {
   testPathIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', '!src/*.d.ts'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 };
