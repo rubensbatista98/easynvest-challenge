9136a87ef0be9e3c8e8d892e2b565e4712b9bf7d
Rubens Batista <batistarubinho@gmail.com>
2021-09-09 16:46:21 -0300

    feat: insert pages in app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+import { Home } from 'pages/home';
+import { Register } from 'pages/register';
+
+import { Router, setupSpa } from 'utils/custom-spa';
+
 import 'styles/global.module.css';
 
-const app = document.querySelector<HTMLDivElement>('#app');
+function init() {
+  const $app = document.querySelector<HTMLDivElement>('#app');
 
-if (app) {
-  app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+  if ($app === null) return;
+
+  setupSpa();
+
+  Router($app, {
+    '/': Home(),
+    '/cadastrar': Register()
+  });
 }
+
+init();
