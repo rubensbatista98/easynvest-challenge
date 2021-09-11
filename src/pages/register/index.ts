9dd81c0b5a331744777d7e2a9c94259c31c44462
Rubens Batista <batistarubinho@gmail.com>
2021-09-11 15:56:34 -0300

    feat: add styles for some peaces of the register page

--- src/pages/register/index.ts
+++ src/pages/register/index.ts
@@ -1,44 +1,56 @@
 import logo from 'assets/logo-icon.svg';
 
+import registerStyles from './styles.module.css';
+
 function Register() {
   const $wrapper = document.createElement('section');
 
+  $wrapper.classList.add(registerStyles.wrapper);
+
   $wrapper.innerHTML = `
-    <aside>
-      <img width="300px" height="200px" src=${logo} alt="" />
+    <aside class=${registerStyles.sidebar}>
+      <img
+        width="300px"
+        height="200px"
+        src=${logo}
+        alt=""
+        class=${registerStyles.logo}
+      />
     </aside>
 
-    <nav>
-      <a href="/" data-link>Ver usuários</a>
-    </nav>
+    <div class=${registerStyles.container}>
+      <nav>
+        <a href="/" data-link>Ver usuários</a>
+      </nav>
 
-    <main>
-      <h1>Cadastrar usuário</h1>
+      <main>
+        <h1 class=${registerStyles.title}>Cadastrar usuário</h1>
 
-      <form>
-        <section>
-          <label for="name">Nome completo (sem abreviações)</label>
-          <input type="text" id="name" name="name" />
-        </section>
+        <form>
+          <section>
+            <label for="name">Nome completo (sem abreviações)</label>
+            <input type="text" id="name" name="name" />
+          </section>
 
-        <section>
-          <label for="email">E-mail</label>
-          <input type="email" id="email" name="email" />
-        </section>
+          <section>
+            <label for="email">E-mail</label>
+            <input type="email" id="email" name="email" />
+          </section>
 
-        <section>
-          <label for="cpf">CPF</label>
-          <input type="number" id="cpf" name="cpf" />
-        </section>
+          <section>
+            <label for="cpf">CPF</label>
+            <input type="number" id="cpf" name="cpf" />
+          </section>
 
-        <section>
-          <label for="phone">Telefone</label>
-          <input type="tel" id="phone" name="phone" />
-        </section>
+          <section>
+            <label for="phone">Telefone</label>
+            <input type="tel" id="phone" name="phone" />
+          </section>
 
-        <button type="submit">Cadastrar</button>
-      </form>
-    </main>
+          <button type="submit">Cadastrar</button>
+        </form>
+      </main>
+    </div>
   `;
 
   return $wrapper;
