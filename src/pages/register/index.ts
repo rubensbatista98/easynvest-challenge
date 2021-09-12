ef8490316719d61abee78cef1b9f8ff03e20295f
Rubens Batista <batistarubinho@gmail.com>
2021-09-12 00:00:46 -0300

    feat: add styles for link and arrow icon

--- src/pages/register/index.ts
+++ src/pages/register/index.ts
@@ -20,7 +20,33 @@ function Register() {
 
     <div class=${registerStyles.container}>
       <nav>
-        <a href="/" data-link>Ver usuários</a>
+        <a href="/" data-link class=${registerStyles.link}>
+          <svg
+            aria-hidden="true"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11 5L4 12L11 19"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M4 12H20"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+
+          Ver usuários
+        </a>
       </nav>
 
       <main>
